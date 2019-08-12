d07f38992718487add88675b6b8a3ec2136fb022
Oswin So <oswinso@gmail.com>
2019-08-12 00:41:06 -0400

    Emptied all files (Answers are in answers branch)

--- igvc_training_exercises/src/week2/exercises/string_subscriber.cpp
+++ igvc_training_exercises/src/week2/exercises/string_subscriber.cpp
@@ -1,17 +1,3 @@
-#include <ros/ros.h>
-#include <std_msgs/String.h>
-
-void integerCallback(std_msgs::String message)
-{
-  ROS_INFO_STREAM("I received a message: " << message.data);
-}
-
 int main(int argc, char** argv)
 {
-  ros::init(argc, argv, "subscriber");
-
-  ros::NodeHandle nh;
-  ros::Subscriber integer_sub = nh.subscribe("my_string", 1, &integerCallback);
-
-  ros::spin();
 }
